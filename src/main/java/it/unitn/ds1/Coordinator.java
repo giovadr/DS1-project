8ecc6bf0f01f8d74b091f3748c80b42eb4d33611
Alessio Faieta <alessio.faieta97@gmail.com>
2021-07-03 17:16:54 +0200

    Add communication between Coordinator and Client
    At the moment, the coordinators responses are fixed, they don't interact with the servers.

--- src/main/java/it/unitn/ds1/Coordinator.java
+++ src/main/java/it/unitn/ds1/Coordinator.java
@@ -33,10 +33,42 @@ public class Coordinator extends AbstractActor{
         System.out.println(servers);
     }
 
+    private void onTxnBeginMsg(Client.TxnBeginMsg msg) {
+        ActorRef currentClient = getSender();
+        currentClient.tell(new Client.TxnAcceptMsg(), getSelf());
+    }
+
+    private void onReadMsg(Client.ReadMsg msg) {
+        ActorRef currentClient = getSender();
+        // TODO: retrieve the value from the server
+        Client.ReadResultMsg readResult = new Client.ReadResultMsg(msg.key, 100);
+        currentClient.tell(readResult, getSelf());
+
+        System.out.println("COORDINATOR " + coordinatorId + " SEND READ RESULT (" + readResult.key + ", " + readResult.value + ") TO CLIENT " + msg.clientId);
+    }
+
+    private void onWriteMsg(Client.WriteMsg msg) {
+        // TODO: write value on the server
+
+        System.out.println("COORDINATOR " + coordinatorId + " WRITE (" + msg.key + ", " + msg.value + ")");
+    }
+
+    private void onTxnEndMsg(Client.TxnEndMsg msg) {
+        ActorRef currentClient = getSender();
+
+        // TODO: decide if commit or abort
+        Client.TxnResultMsg transactionResult = new Client.TxnResultMsg(true);
+        currentClient.tell(transactionResult, getSelf());
+    }
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(WelcomeMsg.class,  this::onWelcomeMsg)
+                .match(Client.TxnBeginMsg.class,  this::onTxnBeginMsg)
+                .match(Client.ReadMsg.class,  this::onReadMsg)
+                .match(Client.WriteMsg.class,  this::onWriteMsg)
+                .match(Client.TxnEndMsg.class,  this::onTxnEndMsg)
                 .build();
     }
 }
