c4f680b87c62beb81cf8cd2010207fb3e14677c8
Alessio Faieta <alessio.faieta97@gmail.com>
2021-07-04 21:11:22 +0200

    Add communication between Coordinator and Server
    The management of the local workspaces in the server is not complete yet: the part for deciding if the server is able to commit or not is not implemented fully.

--- src/main/java/it/unitn/ds1/Coordinator.java
+++ src/main/java/it/unitn/ds1/Coordinator.java
@@ -3,15 +3,13 @@ package it.unitn.ds1;
 import akka.actor.*;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Coordinator extends AbstractActor{
 
     private final Integer coordinatorId;
     private List<ActorRef> servers;
+    private final Map<ActorRef, TransactionInfo> ongoingTransactions = new HashMap<>();
 
     public Coordinator(int coordinatorId) {
         this.coordinatorId = coordinatorId;
@@ -21,10 +19,81 @@ public class Coordinator extends AbstractActor{
         return Props.create(Coordinator.class, () -> new Coordinator(coordinatorId));
     }
 
+    public static class TransactionInfo implements Serializable {
+        public final Set<ActorRef> servers;
+        public Integer nYesVotes;
+        public TransactionInfo() {
+            this.servers = new HashSet<>();
+            this.nYesVotes = 0;
+        }
+    }
+
     public static class WelcomeMsg implements Serializable {
         public final List<ActorRef> servers;
         public WelcomeMsg(List<ActorRef> servers) {
             this.servers = Collections.unmodifiableList(new ArrayList<>(servers));
+        }
+    }
+
+    // READ request from the coordinator to the server
+    public static class ReadMsg implements Serializable {
+        public final ActorRef client;
+        public final Integer key; // the key of the value to read
+        public ReadMsg(ActorRef client, int key) {
+            this.client = client;
+            this.key = key;
+        }
+    }
+
+    // reply from the server when requested a READ on a given key
+    public static class ReadResultMsg implements Serializable {
+        public final ActorRef client; // the client to which forward the result
+        public final Integer key; // the key associated to the requested item
+        public final Integer value; // the value found in the data store for that item
+        public ReadResultMsg(ActorRef client, int key, int value) {
+            this.client = client;
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    // WRITE request from the coordinator to the server
+    public static class WriteMsg implements Serializable {
+        public final ActorRef client;
+        public final Integer key; // the key of the value to write
+        public final Integer value; // the new value to write
+        public WriteMsg(ActorRef client, int key, int value) {
+            this.client = client;
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public enum Vote {NO, YES}
+    public enum Decision {ABORT, COMMIT}
+
+    public static class VoteRequest implements Serializable {
+        public final ActorRef client;
+        public VoteRequest(ActorRef client) {
+            this.client = client;
+        }
+    }
+
+    public static class VoteResponse implements Serializable {
+        public final ActorRef client;
+        public final Vote vote;
+        public VoteResponse(ActorRef client, Vote v) {
+            this.client = client;
+            vote = v;
+        }
+    }
+
+    public static class DecisionMsg implements Serializable {
+        public final ActorRef client;
+        public final Decision decision;
+        public DecisionMsg(ActorRef client, Decision d) {
+            this.client = client;
+            decision = d;
         }
     }
 
@@ -36,19 +105,37 @@ public class Coordinator extends AbstractActor{
     private void onTxnBeginMsg(Client.TxnBeginMsg msg) {
         ActorRef currentClient = getSender();
         currentClient.tell(new Client.TxnAcceptMsg(), getSelf());
+        ensureContactedServersExists(currentClient);
+    }
+
+    private void ensureContactedServersExists(ActorRef client) {
+        if (!ongoingTransactions.containsKey(client)) {
+            ongoingTransactions.put(client, new TransactionInfo());
+        }
+    }
+
+    private void addContactedServer(ActorRef client, ActorRef server) {
+        ongoingTransactions.get(client).servers.add(server);
     }
 
     private void onReadMsg(Client.ReadMsg msg) {
         ActorRef currentClient = getSender();
-        // TODO: retrieve the value from the server
-        Client.ReadResultMsg readResult = new Client.ReadResultMsg(msg.key, 100);
-        currentClient.tell(readResult, getSelf());
+        ActorRef currentServer = getServerFromKey(msg.key);
+        currentServer.tell(new ReadMsg(getSender(), msg.key), getSelf());
+        addContactedServer(currentClient, currentServer);
+    }
 
-        System.out.println("COORDINATOR " + coordinatorId + " SEND READ RESULT (" + readResult.key + ", " + readResult.value + ") TO CLIENT " + msg.clientId);
+    private void onReadResultMsg(ReadResultMsg msg) {
+        msg.client.tell(new Client.ReadResultMsg(msg.key, msg.value), getSelf());
+
+        System.out.println("COORDINATOR " + coordinatorId + " SEND READ RESULT (" + msg.key + ", " + msg.value + ") TO " + msg.client.path().name());
     }
 
     private void onWriteMsg(Client.WriteMsg msg) {
-        // TODO: write value on the server
+        ActorRef currentClient = getSender();
+        ActorRef currentServer = getServerFromKey(msg.key);
+        currentServer.tell(new WriteMsg(currentClient, msg.key, msg.value), getSelf());
+        addContactedServer(currentClient, currentServer);
 
         System.out.println("COORDINATOR " + coordinatorId + " WRITE (" + msg.key + ", " + msg.value + ")");
     }
@@ -56,9 +143,42 @@ public class Coordinator extends AbstractActor{
     private void onTxnEndMsg(Client.TxnEndMsg msg) {
         ActorRef currentClient = getSender();
 
-        // TODO: decide if commit or abort
-        Client.TxnResultMsg transactionResult = new Client.TxnResultMsg(true);
-        currentClient.tell(transactionResult, getSelf());
+        if (msg.commit) {
+            Set<ActorRef> contactedServers = ongoingTransactions.get(currentClient).servers;
+            for(ActorRef server : contactedServers) {
+                server.tell(new VoteRequest(currentClient), getSelf());
+            }
+        } else {
+            currentClient.tell(new Client.TxnResultMsg(false), getSelf());
+            sendDecisionToAllContactedServers(currentClient, Decision.ABORT);
+        }
+    }
+
+    private void onVoteResponseMsg(VoteResponse msg) {
+        TransactionInfo currentTransactionInfo = ongoingTransactions.get(msg.client);
+        if (msg.vote == Vote.YES) {
+            currentTransactionInfo.nYesVotes++;
+
+            if (currentTransactionInfo.nYesVotes == currentTransactionInfo.servers.size()) {
+                msg.client.tell(new Client.TxnResultMsg(true), getSelf());
+                sendDecisionToAllContactedServers(msg.client, Decision.COMMIT);
+            }
+        } else {
+            msg.client.tell(new Client.TxnResultMsg(false), getSelf());
+            sendDecisionToAllContactedServers(msg.client, Decision.ABORT);
+        }
+    }
+
+    private void sendDecisionToAllContactedServers(ActorRef client, Decision decision) {
+        TransactionInfo currentTransactionInfo = ongoingTransactions.get(client);
+        for(ActorRef server : currentTransactionInfo.servers) {
+            server.tell(new DecisionMsg(client, decision), getSelf());
+        }
+    }
+
+    private ActorRef getServerFromKey(int key) {
+        int serverId = key / 10;
+        return servers.get(serverId);
     }
 
     @Override
@@ -67,8 +187,10 @@ public class Coordinator extends AbstractActor{
                 .match(WelcomeMsg.class,  this::onWelcomeMsg)
                 .match(Client.TxnBeginMsg.class,  this::onTxnBeginMsg)
                 .match(Client.ReadMsg.class,  this::onReadMsg)
+                .match(ReadResultMsg.class,  this::onReadResultMsg)
                 .match(Client.WriteMsg.class,  this::onWriteMsg)
                 .match(Client.TxnEndMsg.class,  this::onTxnEndMsg)
+                .match(VoteResponse.class,  this::onVoteResponseMsg)
                 .build();
     }
 }
--- src/main/java/it/unitn/ds1/Server.java
+++ src/main/java/it/unitn/ds1/Server.java
@@ -5,25 +5,98 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Server extends AbstractActor {
 
     private final Integer serverId;
+    private final DataEntry[] data = new DataEntry[Main.N_KEYS_PER_SERVER];
+    private final Map<ActorRef, DataEntry[]> localWorkspaces = new HashMap<>();
+
+    public static class DataEntry {
+        public final Integer version;
+        public final Integer value;
+        public DataEntry(int version, int value) {
+            this.version = version;
+            this.value = value;
+        }
+    }
 
     public Server(int serverId) {
         this.serverId = serverId;
+        Arrays.fill(data, new DataEntry(0, 100));
     }
 
     static public Props props(int serverId) {
         return Props.create(Server.class, () -> new Server(serverId));
     }
 
+    private void onReadMsg(Coordinator.ReadMsg msg) {
+        ensureLocalWorkspaceExists(msg.client);
+
+        DataEntry[] localWorkspace = localWorkspaces.get(msg.client);
+        Coordinator.ReadResultMsg readResult = new Coordinator.ReadResultMsg(msg.client, msg.key, localWorkspace[msg.key % 10].value);
+
+        ActorRef currentCoordinator = getSender();
+        currentCoordinator.tell(readResult, getSelf());
+
+        System.out.println("SERVER " + serverId + " SEND READ RESULT (" + readResult.key + ", " + readResult.value + ") TO COORDINATOR");
+    }
+
+    private void onWriteMsg(Coordinator.WriteMsg msg) {
+        ensureLocalWorkspaceExists(msg.client);
+
+        DataEntry[] localWorkspace = localWorkspaces.get(msg.client);
+        DataEntry previousDataEntry = localWorkspace[msg.key % 10];
+        localWorkspace[msg.key % 10] = new DataEntry(previousDataEntry.version, msg.value);
+
+        System.out.println("SERVER " + serverId + " WRITE (" + msg.key + ", " + localWorkspace[msg.key % 10].value + ")");
+    }
+
+    private void onVoteRequestMsg(Coordinator.VoteRequest msg) {
+        DataEntry[] localWorkspace = localWorkspaces.get(msg.client);
+        boolean localWorkspaceEqualsData = true;
+
+        // TODO: implement locks
+        // TODO: check only accessed values
+        for (int i = 0; i < Main.N_KEYS_PER_SERVER; i++) {
+            if (!localWorkspace[i].version.equals(data[i].version)) {
+                localWorkspaceEqualsData = false;
+                break;
+            }
+        }
+
+        Coordinator.Vote vote;
+        if (localWorkspaceEqualsData) {
+            vote = Coordinator.Vote.YES;
+        } else {
+            vote = Coordinator.Vote.NO;
+        }
+
+        ActorRef currentCoordinator = getSender();
+        currentCoordinator.tell(new Coordinator.VoteResponse(msg.client, vote), getSelf());
+    }
+
+    private void onDecisionMsg(Coordinator.DecisionMsg msg) {
+        if (msg.decision == Coordinator.Decision.ABORT) {
+            localWorkspaces.remove(msg.client);
+        }
+        // TODO: manage the case of Decision.COMMIT
+    }
+
+    private void ensureLocalWorkspaceExists(ActorRef client) {
+        if (!localWorkspaces.containsKey(client)) {
+            localWorkspaces.put(client, Arrays.copyOf(data, 10));
+        }
+    }
+
     @Override
     public AbstractActor.Receive createReceive() {
         return receiveBuilder()
+                .match(Coordinator.ReadMsg.class,  this::onReadMsg)
+                .match(Coordinator.WriteMsg.class, this::onWriteMsg)
+                .match(Coordinator.VoteRequest.class, this::onVoteRequestMsg)
+                .match(Coordinator.DecisionMsg.class, this::onDecisionMsg)
                 .build();
     }
 }
